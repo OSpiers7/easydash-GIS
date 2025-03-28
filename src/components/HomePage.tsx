fd57bac4209d92a37904f235b4cda4ae5adf791d
nduvall12 <nickduvall03@gmail.com>
2025-03-28 09:49:13 -0400

    updated delete button styling cause I didn't like it

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -49,51 +49,36 @@ const HomePage: React.FC<HomePageProps> = ({ onSelectDashboard }) => {
         setKeys(getAllLocalStorageKeys());
     }, []);
 
-    return (
-        <div className="dashboard">
-            <HomeTopBanner />
-            <div className="button-container">
-            <button 
-                    className="storage-button" 
-                    onClick={() => handleButtonClick("DefaultValue")}
-                >
-                    +
-                </button>
-                {keys.map((key, index) => (
-  <div key={key} style={{ position: "relative", display: "inline-block" }}>
-    <button 
-      className="storage-button" 
-      onClick={() => handleButtonClick(key)}
-    >
-      {key}
-    </button>
-      <button 
-        onClick={() => handleDeleteKey(key)}
-        style={{
-          position: "absolute",
-          top: "5px",
-          right: "5px",
-          background: "red",
-          color: "white",
-          borderRadius: "50%",
-          border: "none",
-          width: "20px",
-          height: "20px",
-          cursor: "pointer",
-          fontSize: "12px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-        title={`Delete "${key}"`}
-      >
-        ✕
-      </button>
-  </div>
-))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashboard">
+      <HomeTopBanner />
+      <div className="button-container">
+        <button
+          className="storage-button"
+          onClick={() => handleButtonClick("DefaultValue")}
+        >
+          +
+        </button>
+        {keys.map((key, index) => (
+          <div key={key} style={{ position: "relative", display: "inline-block" }}>
+            <button
+              className="storage-button"
+              onClick={() => handleButtonClick(key)}
+            >
+              {key}
+            </button>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteKey(key)}
+              title={`Delete "${key}"`}
+            >
+              ✕
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
